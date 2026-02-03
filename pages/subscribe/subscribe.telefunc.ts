df0c5597ff2bd68d68d17f62a753dbd9dcd5092a
MaximeCode <contact.mbaude@gmail.com>
2026-02-03 16:45:51 +0100

    a retester

--- pages/subscribe/subscribe.telefunc.ts
+++ pages/subscribe/subscribe.telefunc.ts
@@ -1,19 +1,23 @@
 const argon2 = require('argon2');
-// import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from '@prisma/client';
+
 
 export async function onSubscribe(data: { email: string; password: string }) {
+  const prisma = new PrismaClient({
+    datasourceUrl: process.env.DATABASE_URL,
+  });
   console.log("Côté backend", data);
-  //   try {
-  //     const hashedPassword = await argon2.hash(data.password);
-  //     const user = await prisma.user.create({
-  //       data: {
-  //         email: data.email,
-  //         password: hashedPassword,
-  //       },
-  //     });
-  //     return { success: true, user };
-  //   } catch (error) {
-  //     console.error("Error hashing password", error);
-  //     return { success: false, error: "Error hashing password" };
-  // }
+  try {
+    const hashedPassword = await argon2.hash(data.password);
+    const user = await prisma.user.create({
+      data: {
+        email: data.email,
+        password: hashedPassword,
+      },
+    });
+    return { success: true, user };
+  } catch (error) {
+    console.error("Error hashing password", error);
+    return { success: false, error: "Error hashing password" };
+  }
 } 
