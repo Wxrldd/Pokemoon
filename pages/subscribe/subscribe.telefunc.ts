8a26138f7b565ef30a91005e3c20bea17454bc83
MaximeCode <contact.mbaude@gmail.com>
2026-02-04 15:12:03 +0100

    Subscribe OK

--- pages/subscribe/subscribe.telefunc.ts
+++ pages/subscribe/subscribe.telefunc.ts
@@ -9,15 +9,17 @@ export async function onSubscribe(data: { email: string; password: string }) {
   console.log("Côté backend", data);
   try {
     const hashedPassword = await argon2.hash(data.password);
+    console.log("Hashed password", hashedPassword);
     const user = await prisma.user.create({
       data: {
         email: data.email,
         password: hashedPassword,
       },
     });
+    console.log("User created", user);
     return { success: true, user };
   } catch (error) {
     console.error("Error hashing password", error);
     return { success: false, error: "Error hashing password" };
   }
-} 
+}
